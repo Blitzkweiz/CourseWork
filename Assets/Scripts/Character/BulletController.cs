8a57b11259fb01aa91ff3510628cda9ab26944c6
Blitzkweiz <bezlikiy.pianist@gmail.com>
2020-05-23 13:38:19 +0300

    Welcome my dear

---
+++ Assets/Scripts/Character/BulletController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+
+    public float lifeTime;
+    public GameObject shooter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DeathDelay());
+    }
+
+    private IEnumerator DeathDelay()
+    {
+        yield return new WaitForSeconds(lifeTime);
+        Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
+        Vector2 newPosition = currentPosition + GetComponent<Rigidbody2D>().velocity * Time.deltaTime;
+
+        RaycastHit2D[] hits = Physics2D.LinecastAll(currentPosition, newPosition);
+
+        foreach (RaycastHit2D hit in hits)
+        {
+            GameObject other = hit.collider.gameObject;
+
+            if (other.CompareTag("Wall"))
+            {
+                Destroy(gameObject);
+            }
+
+            if (shooter.CompareTag("Player") && other.CompareTag("Enemy"))
+            {
+                var enemy = other.GetComponent<CharacterController>();
+                enemy.TakeDamage(shooter.GetComponent<CharacterController>().damage);
+                Destroy(gameObject);
+            }
+
+            if (shooter.CompareTag("Enemy") && other.CompareTag("Player"))
+            {
+                var player = other.GetComponent<CharacterController>();
+                player.TakeDamage(shooter.GetComponent<CharacterController>().damage);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Interactions/TrapController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapController : MonoBehaviour
+{
+    public int damage;
+    private CharacterController characterController;
+    private float lastDamage;
+    public float damageDelay;
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if(col.CompareTag("Player") && lastDamage + damageDelay < Time.time && col.gameObject.TryGetComponent<CharacterController>(out characterController))
+        {
+            characterController.TakeDamage(damage);
+            lastDamage = Time.time;
+        }
+    }
+}
